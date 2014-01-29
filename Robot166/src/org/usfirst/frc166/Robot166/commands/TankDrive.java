ae03f6a676be384852da0b2f55f113618185184d
Andrew <liv4waterville@gmail.com>
2014-01-28 19:17:32 -0500

    I fixed previous errors in my code, ie. not even calling function...

--- Robot166/src/org/usfirst/frc166/Robot166/commands/TankDrive.java
+++ Robot166/src/org/usfirst/frc166/Robot166/commands/TankDrive.java
@@ -32,6 +32,7 @@ public class TankDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.drive.TankDrive4();
     }
 
     // Make this return true when this Command no longer needs to run execute()
