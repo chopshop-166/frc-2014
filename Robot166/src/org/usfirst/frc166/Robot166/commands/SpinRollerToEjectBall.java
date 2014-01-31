1f38f84e33467f375555b87e24900215229897a0
Austin Brancheau <Austin.Brancheau@gmail.com>
2014-01-30 20:10:00 -0500

    Roller reformatted

--- Robot166/src/org/usfirst/frc166/Robot166/commands/SpinRollerToEjectBall.java
+++ Robot166/src/org/usfirst/frc166/Robot166/commands/SpinRollerToEjectBall.java
@@ -29,16 +29,17 @@ public class SpinRollerToEjectBall extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.roller.backwards();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Robot.roller.backwards();
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
@@ -48,5 +49,6 @@ public class SpinRollerToEjectBall extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+
     }
 }
