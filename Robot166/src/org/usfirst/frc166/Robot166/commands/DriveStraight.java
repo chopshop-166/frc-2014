63f92719efd6ddc20e30da2cad65a5ce32bddfd1
Andrew <liv4waterville@gmail.com>
2014-02-09 15:55:26 -0500

    Tested JoytickDrive, Works. Transmission works, but is reversed.

--- Robot166/src/org/usfirst/frc166/Robot166/commands/DriveStraight.java
+++ Robot166/src/org/usfirst/frc166/Robot166/commands/DriveStraight.java
@@ -28,11 +28,12 @@ public class DriveStraight extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.drive.DriveStraight();
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.drive.DriveStraight();
     }
 
     // Make this return true when this Command no longer needs to run execute()
