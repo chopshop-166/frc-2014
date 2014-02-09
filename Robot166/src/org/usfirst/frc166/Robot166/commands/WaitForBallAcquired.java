7cec3b6a2c7dfd3ba34dad3e7c37817c9ebb4a4e
Austin Brancheau <Austin.Brancheau@gmail.com>
2014-02-09 13:07:02 -0500

    setup to work as wait function

--- Robot166/src/org/usfirst/frc166/Robot166/commands/WaitForBallAcquired.java
+++ Robot166/src/org/usfirst/frc166/Robot166/commands/WaitForBallAcquired.java
@@ -31,12 +31,12 @@ public class WaitForBallAcquired extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Robot.roller.stopRolling();
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.shoulder.isBallAcquired();
     }
 
     // Called once after isFinished returns true
