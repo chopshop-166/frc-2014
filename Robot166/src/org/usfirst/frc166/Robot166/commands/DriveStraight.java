f0096d5b67f15dd8b5e47146ed1a4cbbd963c225
Dan Mulligan <danmulligan133@gmail.com>
2014-03-05 23:42:06 -0500

    changed driveStraight's interrupt to return to default command

--- Robot166/src/org/usfirst/frc166/Robot166/commands/DriveStraight.java
+++ Robot166/src/org/usfirst/frc166/Robot166/commands/DriveStraight.java
@@ -43,6 +43,6 @@ public class DriveStraight extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-        end();
+        Robot.drive.initDefaultCommand();
     }
 }
