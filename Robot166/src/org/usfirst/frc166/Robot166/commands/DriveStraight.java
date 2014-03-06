5cb718d409261a7a1968a01343ed091fefef72b2
Dan Mulligan <danmulligan133@gmail.com>
2014-03-05 22:57:53 -0500

    added stop motors to driveStraight's end

--- Robot166/src/org/usfirst/frc166/Robot166/commands/DriveStraight.java
+++ Robot166/src/org/usfirst/frc166/Robot166/commands/DriveStraight.java
@@ -38,9 +38,11 @@ public class DriveStraight extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.drive.stopMotors();
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
